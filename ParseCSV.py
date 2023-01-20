7f6b59e2e0abda115382edf4a7e17a237e34a7ee
Ricardo-Ricardo <92102618+Ricardo-Ricardo@users.noreply.github.com>
2023-01-19 18:21:40 -0600

    changes the commas to dashes in csv file

--- ParseCSV.py
+++ ParseCSV.py
@@ -1,8 +1,11 @@
 import csv
 
-with open('names.csv', 'r') as csv_file:
-    csv_reader = csv.reader(csv_file)
+with open('names.csv', 'r') as csv_file: #open original file
+    csv_reader = csv.reader(csv_file) # read original file
 
-    next(csv_reader) # skips the first line
-    for line in csv_reader:
-        print(line[2])
+    #next(csv_reader) # skips the first line
+    with open('new_names.csv', 'w') as new_file: # open new file
+        csv_writer = csv.writer(new_file, delimiter = '-') # adds a dash to each line instead of comma
+
+        for line in csv_reader: # reads each line
+            csv_writer.writerow(line) # wirte out each new line
