6bb69981de9fda40b56d8df2141c2b822c514fbd
Ricardo-Ricardo <92102618+Ricardo-Ricardo@users.noreply.github.com>
2023-01-19 18:03:56 -0600

    sorting files by renaming

---
+++ SortFilesByNumber.py
@@ -0,0 +1,26 @@
+### this script is an example of sorting files by renaming 
+
+# Example files to sort:
+# Earth - Our Solar System - #4.mp4
+# The Sun - Our Solar System - #1.mp4
+# Venus - Our Solar System - #3.mp4
+# Mercury - Our Solar System - #2.mp4
+
+import os
+
+# Use directory where all the files are stores
+os.chdir('/Users/ricky/Desktop/Videos')
+
+for f in os.listdir():
+    f_name, f_ext = os.path.splitext(f) # tuple of the name and the type of file/extension
+    if (f_ext == ".mp4"): # checks the file type so only wanted files are changed
+        f_title, f_course, f_num = f_name.split('-') #tuple of the 3 sections, title, course, and number
+        f_title = f_title.strip()
+        f_course = f_course.strip()
+        f_num = f_num.strip()[1:].zfill(2) # zero padded string so 10 won't be before 2
+        
+        print('{}-{}{}'.format(f_num, f_title, f_ext))
+    else:
+        continue
+
+
