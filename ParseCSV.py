e11e87cca0b0455fca60c7fb4842ea04837d86bf
Ricardo-Ricardo <92102618+Ricardo-Ricardo@users.noreply.github.com>
2023-01-19 18:33:24 -0600

    added dictionary to make it easier to separate each part

--- ParseCSV.py
+++ ParseCSV.py
@@ -1,11 +1,15 @@
 import csv
 
 with open('names.csv', 'r') as csv_file: #open original file
-    csv_reader = csv.reader(csv_file) # read original file
+    csv_reader = csv.DictReader(csv_file) # read original file
 
     #next(csv_reader) # skips the first line
     with open('new_names.csv', 'w') as new_file: # open new file
-        csv_writer = csv.writer(new_file, delimiter = '-') # adds a dash to each line instead of comma
+        fieldnames = ['first_name', 'last_name']
+        csv_writer = csv.DictWriter(new_file, fieldnames = fieldnames, delimiter = '\t') # adds a tab to each line instead of comma
+
+        csv_writer.writeheader()
 
         for line in csv_reader: # reads each line
-            csv_writer.writerow(line) # wirte out each new line
+            del line['email']
+            csv_writer.writerow(line) # write out each new line
