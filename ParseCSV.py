c4d4fbbe675be2171d59c8b2eb798f9562d44648
Ricardo-Ricardo <92102618+Ricardo-Ricardo@users.noreply.github.com>
2023-01-19 18:36:29 -0600

    added comments

--- ParseCSV.py
+++ ParseCSV.py
@@ -1,3 +1,10 @@
+### this script is an example of how to read, parse, and write csv files
+
+# example of csv file
+# first_name,last_name,email
+# John,Doe,john-doe@hotmail.com
+# Chris,Smith,ChrisSmith@gmail.com
+
 import csv
 
 with open('names.csv', 'r') as csv_file: #open original file
@@ -5,11 +12,11 @@ with open('names.csv', 'r') as csv_file: #open original file
 
     #next(csv_reader) # skips the first line
     with open('new_names.csv', 'w') as new_file: # open new file
-        fieldnames = ['first_name', 'last_name']
+        fieldnames = ['first_name', 'last_name'] # Dictionary to store key values
         csv_writer = csv.DictWriter(new_file, fieldnames = fieldnames, delimiter = '\t') # adds a tab to each line instead of comma
 
         csv_writer.writeheader()
 
         for line in csv_reader: # reads each line
-            del line['email']
+            del line['email'] # deletes the last line of emails
             csv_writer.writerow(line) # write out each new line
